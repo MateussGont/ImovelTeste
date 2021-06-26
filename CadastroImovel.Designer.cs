d015731f20ce2d54c7f260a465b4954b3b45fd1a
MateussGont <mateussilvagontijo@gmail.com>
2021-06-26 15:00:10 -0300

    Initial

---
+++ CadastroImovel.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace ImovelTeste
+{
+    partial class CadastroImovel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtCEP = new System.Windows.Forms.MaskedTextBox();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.txtRua = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtUF = new System.Windows.Forms.TextBox();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtNomeP = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Salvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(203, 62);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(18, 13);
+            this.labelID.TabIndex = 1;
+            this.labelID.Text = "ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(294, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome do Propietario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(338, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CEP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Bairro";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Rua";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(84, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Cidade";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(84, 202);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "UF";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(84, 228);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Numero";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(294, 117);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Preço";
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(372, 59);
+            this.txtCEP.Mask = "00000-999";
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(100, 20);
+            this.txtCEP.TabIndex = 10;
+            this.txtCEP.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(341, 114);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(80, 20);
+            this.txtPreco.TabIndex = 11;
+            // 
+            // txtRua
+            // 
+            this.txtRua.Location = new System.Drawing.Point(117, 117);
+            this.txtRua.Name = "txtRua";
+            this.txtRua.Size = new System.Drawing.Size(131, 20);
+            this.txtRua.TabIndex = 12;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(124, 143);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(124, 20);
+            this.txtBairro.TabIndex = 13;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(130, 169);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(118, 20);
+            this.txtCidade.TabIndex = 14;
+            // 
+            // txtUF
+            // 
+            this.txtUF.Location = new System.Drawing.Point(134, 199);
+            this.txtUF.Name = "txtUF";
+            this.txtUF.Size = new System.Drawing.Size(34, 20);
+            this.txtUF.TabIndex = 15;
+            this.txtUF.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(134, 225);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(34, 20);
+            this.txtNum.TabIndex = 16;
+            // 
+            // txtNomeP
+            // 
+            this.txtNomeP.Location = new System.Drawing.Point(403, 140);
+            this.txtNomeP.Name = "txtNomeP";
+            this.txtNomeP.Size = new System.Drawing.Size(135, 20);
+            this.txtNomeP.TabIndex = 17;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(227, 59);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(488, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Confirma";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Salvar
+            // 
+            this.Salvar.Location = new System.Drawing.Point(397, 275);
+            this.Salvar.Name = "Salvar";
+            this.Salvar.Size = new System.Drawing.Size(75, 23);
+            this.Salvar.TabIndex = 20;
+            this.Salvar.Text = "Cadastrar";
+            this.Salvar.UseVisualStyleBackColor = true;
+            this.Salvar.Click += new System.EventHandler(this.Salvar_Click);
+            // 
+            // CadastroImovel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Salvar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtNomeP);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.txtUF);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.txtRua);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelID);
+            this.Name = "CadastroImovel";
+            this.Text = "CadastroImovel";
+            this.Load += new System.EventHandler(this.CadastroImovel_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MaskedTextBox txtCEP;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.TextBox txtRua;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtUF;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtNomeP;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Salvar;
+    }
+}
---
+++ CadastroImovel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.
+using Correios;
+using System.Data.SqlClient;
+
+namespace ImovelTeste
+{
+    public partial class CadastroImovel : Form
+    {
+        public CadastroImovel()
+        {
+            InitializeComponent();
+        }
+        private void CadastroImovel_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            
+
+        }
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCEP.Text) || txtCEP.Text == "     -")
+            {
+                MessageBox.Show("O campo CEP esta vazio!", "Atenção!", MessageBoxButtons.OK);
+            }
+            else
+            {
+                try
+                {
+                    CorreiosApi correiosApi = new CorreiosApi();
+                    var retorno = correiosApi.consultaCEP(txtCEP.Text);
+                    txtRua.Text = retorno.end;
+                    txtBairro.Text = retorno.bairro;
+                    txtCidade.Text = retorno.cidade;
+                    txtUF.Text = retorno.uf;
+                }
+                catch(Exception invalid)
+                {
+                    MessageBox.Show("O CEP inserido é invalido!", "Atenção!", MessageBoxButtons.OK)
+                }
+            }
+        }
+
+        private void Salvar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
